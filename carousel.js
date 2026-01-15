d92b860286bc245dc9ddc688a3ed9ad79b8ce84e
gloria-byte-mc <gloriamachoka1@gmail.com>
2026-01-15 03:18:35 +0300

    changes

--- carousel.js
+++ carousel.js
@@ -1,5 +1,5 @@
 
-    // Array of background images
+   
     const images = [
       'images/African-storytelling.jpg',
       'images/zulu.jpg',
@@ -10,22 +10,20 @@
     let currentIndex = 0;
     const side1 = document.querySelector('.side1');
 
-    // Set initial background
     side1.style.backgroundImage = `url('${images[currentIndex]}')`;
 
-    // Function to change background
+   
     function changeBackground() {
       currentIndex++;
       
-      // If we've reached the end, go back to the first image
+     
       if (currentIndex >= images.length) {
         currentIndex = 0;
       }
       
-      // Update background image
       side1.style.backgroundImage = `url('${images[currentIndex]}')`;
     }
 
-    // Change background every 5 seconds (5000 milliseconds)
+    
     setInterval(changeBackground, 20000);
  
